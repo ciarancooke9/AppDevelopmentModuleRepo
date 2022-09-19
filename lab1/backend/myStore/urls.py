a73984f4ec5190c59a6c2146ed1bb0f1140adcf5
Ciaran Cooke <ciaran.cooke9@mail.dcu.ie>
2022-09-19 13:38:02 +0100

    products showing on homepage

--- lab1/backend/myStore/urls.py
+++ lab1/backend/myStore/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('', views.index, name="index")
+   path('', views.index, name="index"),
+   path('products', views.all_products, name="products"),
+   path('products/<int:product_id>', views.product_individual, name="individual_product" ),
 ]
--- lab1/backend/myStore/views.py
+++ lab1/backend/myStore/views.py
@@ -1,5 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import *
 
 def index(request):
-    return render(request, 'index.html')
+    products = Product.objects.all()
+    return render(request, 'index.html', {'products':products})
+
+def all_products(request):
+    products = Product.objects.all()
+    return render(request, 'index.html', {'products':products})
+
+def product_individual(request, product_id):
+    product = Product.objects.get(id=product_id)
+    return render(request, 'product.html', {'products':product})
