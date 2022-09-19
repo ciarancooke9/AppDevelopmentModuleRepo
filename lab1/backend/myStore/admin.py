56f0461ddf83d16759f4a23d7ba70c852cf3fe3e
Ciaran Cooke <ciaran.cooke9@mail.dcu.ie>
2022-09-19 12:57:45 +0100

    New models registered

--- lab1/backend/myStore/admin.py
+++ lab1/backend/myStore/admin.py
@@ -4,3 +4,6 @@ from .models import *
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Product)
+admin.site.register(Basket)
+admin.site.register(Order)
+admin.site.register(BasketItems)
